0391ca28d7dda2dc8b32b45fcbcbe36883d6c135
Henry Zhang <zhanghui629@gmail.com>
2019-09-19 17:27:19 +0800

    update foreach

--- foreach.cpp
+++ foreach.cpp
@@ -1,4 +1,25 @@
 #include <iostream>
+#include <vector>
+
+class Foo {
+friend std::ostream& operator<<(std::ostream&, const Foo&);
+public:
+	Foo() = default;
+	Foo(int foo): i(foo) { std::cout << "ctor" << std::endl; }
+	Foo(const Foo& f) {
+		std::cout << "copy constructor" << std::endl;
+		i = f.i;
+	}
+	void print() { std::cout << i << std::endl; }
+private:
+	int i;
+};
+
+std::ostream& operator<<(std::ostream& os, const Foo& f)
+{
+	os << f.i;
+	return os;
+}
 
 int main(int argc, char const *argv[])
 {
@@ -6,8 +27,15 @@ int main(int argc, char const *argv[])
 
 	std::cout << "numbers:" << std::endl;
 
-	for(auto number : numbers) {
+	for (auto number : numbers)
+	{
 		std::cout << number << std::endl;
+	}
+
+	std::vector<Foo> c = { Foo(4), Foo(3), Foo(2), Foo(1) };
+	// for (auto item : c) {
+	for (auto& item : c) {
+		std::cout << item << std::endl;
 	}
 
 	return 0;

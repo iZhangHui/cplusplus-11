f749708e22c741d7fb55d791b37e5decbe19164d
Henry Zhang <zhanghui629@gmail.com>
2019-10-14 10:32:23 +0800

    pthread affinity

--- threads-affinity/set-affinity.cpp
+++ threads-affinity/set-affinity.cpp
@@ -9,33 +9,35 @@
 
 int main(int argc, char const *argv[])
 {
-	constexpr uint32_t num_threads = 4;
-	std::mutex iomutex;
-	std::vector<std::thread> threads(num_threads);
+    constexpr uint32_t num_threads = 4;
+    std::mutex iomutex;
+    std::vector<std::thread> threads(num_threads);
 
-	for (auto i = 0; i < num_threads; ++i) {
-		threads[i] = std::thread([&iomutex, i]() {
-			while (true) {
-				std::this_thread::sleep_for(std::chrono::milliseconds(900));
+    for (auto i = 0; i < num_threads; ++i) {
+        threads[i] = std::thread([&iomutex, i]() {
+            while (true) {
+                std::this_thread::sleep_for(std::chrono::milliseconds(900));
 
-				std::lock_guard<std::mutex> iolock(iomutex);
-				std::cout << "Thread #" << i << ": on CPU" << sched_getcpu() << "\n";
-			}
-		});
+                std::lock_guard<std::mutex> iolock(iomutex);
+                std::cout << "Thread #" << i << ": on CPU" << sched_getcpu() << "\n";
+                // #include <sched.h>
+                // int sched_getcpu(void); determine CPU on which the calling thread is running
+            }
+        });
 
-		cpu_set_t cpuset;
-		CPU_ZERO(&cpuset);
-		CPU_SET(i, &cpuset);
-		int rc = pthread_setaffinity_np(threads[i].native_handle(),
-										sizeof(cpu_set_t), &cpuset);
-		if (rc != 0) {
-			std::cerr << "Error calling pthread_setaffinity_np: " << rc << "\n";
-		}
-	}
+        cpu_set_t cpuset; // local variable!!!
+        CPU_ZERO(&cpuset);
+        CPU_SET(i, &cpuset);
+        int rc = pthread_setaffinity_np(threads[i].native_handle(),
+                                        sizeof(cpu_set_t), &cpuset);
+        if (rc != 0) {
+            std::cerr << "Error calling pthread_setaffinity_np: " << rc << "\n";
+        }
+    }
 
-	for (auto& t : threads) {
-		t.join();
-	}
+    for (auto &t : threads) {
+        t.join();
+    }
 
-	return 0;
+    return 0;
 }
